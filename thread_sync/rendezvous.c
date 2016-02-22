2356779e5a2e610223b1e31e2e18a6ebc7c1cc0a
knikamo <karolinanikamo@nl106-134-73.student.uu.se>
2016-02-22 11:24:01 +0100

    rendezvous.c

--- thread_sync/rendezvous.c
+++ thread_sync/rendezvous.c
@@ -13,11 +13,14 @@
 #include <unistd.h>    /* sleep() */
 #include <semaphore.h> /* sem_...() */
 #include <pthread.h>   /* pthread_...() */
+#include <errno.h>     /* errno */
 
 #define LOOPS 5
 #define NTHREADS 3
 #define MAX_SLEEP_TIME 3
 
+sem_t sem1;
+sem_t sem2;
 
 /* TODO: Make the two threads perform their iterations in a
  * predictable way. Both should perform iteration 1 before iteration 2
@@ -29,10 +32,13 @@ threadA(void *param __attribute__((unused)))
     int i;
 
     for (i = 0; i < LOOPS; i++) {
-
+        sem_wait(&sem1);
+            //int value;
+            //sem_getvalue(&sem, &value);
+        
 	printf("threadA --> %d iteration\n", i);
 	sleep(rand() % MAX_SLEEP_TIME);
-
+        sem_post(&sem2);
     }
 
     pthread_exit(0);
@@ -43,13 +49,13 @@ void *
 threadB(void *param  __attribute__((unused)))
 {
     int i;
-
+ 
     for (i = 0; i < LOOPS; i++) {
-
+        sem_wait(&sem2);
 
 	printf("threadB --> %d iteration\n", i);
 	sleep(rand() % MAX_SLEEP_TIME);
-
+        sem_post(&sem1);
     }
 
     pthread_exit(0);
@@ -58,10 +64,16 @@ threadB(void *param  __attribute__((unused)))
 int
 main()
 {
+    int hej = sem_init(&sem, 0, 3);
+    printf("The return value is %d\n", hej);
+    printf("errno says: %d\n", errno);
     pthread_t tidA, tidB;
-
+    
     srand(time(NULL));
     pthread_setconcurrency(3);
+    int value; 
+    sem_getvalue(&sem, &value); 
+    printf("The value of the semaphors is %d\n", value);
 
     if (pthread_create(&tidA, NULL, threadA, NULL) ||
 	pthread_create(&tidB, NULL, threadB, NULL)) {
